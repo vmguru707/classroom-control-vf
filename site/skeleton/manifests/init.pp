f1c55829ccd989ce03d1a75111d1ed76108671fc
vmguru707 <ricardo_707@yahoo.com>
2016-05-24 10:08:33 -0700

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,17 @@
+class skeleton {
+   file { '/etc/skel':
+   ensure => directory,
+   owner  => 'root',
+   group  => 'root',
+   mode   => '0755',
+}
+file { '/etc/skel/.bashrc':
+   ensure => file,
+   owner => 'root',
+   group => 'root',
+   mode => '0644',
+   source => 'puppet:///modules/skeleton/bashrc',
+   }
+}
+#Exercise 9.3: Proposed Solution
+#VirtualFundamentals
