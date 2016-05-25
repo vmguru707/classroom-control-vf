5398ec356515a2a8f2c52b3d41db73a62eb0c8ca
vmguru707 <ricardo_707@yahoo.com>
2016-05-25 08:57:32 -0700

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,12 +1,6 @@
 class users::admins {
- users::managed_user { 'joe': }
- users::managed_user { 'alice':
-   group => 'staff',
+ users::managed_user { 'jose': }
+ users::managed_user { 'alice': }
+ users::managed_user { 'chen': }
 }
-users::managed_user { 'aaron':
-   group => 'staff',
-}
-group { 'staff':
-   ensure => present,
- }
-}
+
