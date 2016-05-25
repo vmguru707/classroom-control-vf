0edfc3afd887b034f0362586c5ce02038d82b337
vmguru707 <ricardo_707@yahoo.com>
2016-05-25 06:01:39 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,10 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
+  if $::virtual != 'physical' {
+     $vmname = capitalize($::virtual)
+     notify { "This is a ${vmname} virtual machine.": }
+  
   file { 'motd':
     path    => '/etc/motd',
     ensure  => file,
