0b9c536c79fca47e6a29b28003c47a2d823b0028
vmguru707 <ricardo_707@yahoo.com>
2016-05-26 06:18:42 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,41 +1,61 @@
 class nginx {
- File {
-   owner => 'root',
-   group => 'root',
+case $::osfamily {
+ 'redhat','debian' : {
+   $package = 'nginx'
+   $owner = 'root'
+   $group = 'root'
+   $docroot = '/var/www'
+   $confdir = '/etc/nginx'
+   $logdir = '/var/log/nginx'
+}
+'windows' : {
+   $package = 'nginx-service'
+   $owner = 'Administrator'
+   $group = 'Administrators'
+   $docroot = 'C:/ProgramData/nginx/html'
+   $confdir = 'C:/ProgramData/nginx'
+   $logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+  fail("Module ${module_name} is not supported on ${::osfamily}")
+ }
+}
+
+# user the service will run as. Used in the nginx.conf.erb template
+$user = $::osfamily ? {
+ 'redhat' => 'nginx',
+ 'debian' => 'www-data',
+ 'windows' => 'nobody',
+}
+File {
+   owner => $owner,
+   group => $group,
    mode => '0664',
 }
-   package { 'nginx':
+package { $package:
    ensure => present,
 }
-file { '/var/www':
+file { [ $docroot, "${confdir}/conf.d" ]:
    ensure => directory,
 }
-file { '/var/www/index.html':
+file { "${docroot}/index.html":
    ensure => file,
    source => 'puppet:///modules/nginx/index.html',
 }
-file { '/etc/nginx/nginx.conf':
+file { "${confdir}/nginx.conf":
    ensure => file,
-   source => 'puppet:///modules/nginx/nginx.conf',
-   require => Package['nginx'],
+   content => template('nginx/nginx.conf.erb'),
    notify => Service['nginx'],
 }
-file { '/etc/nginx/conf.d':
-   ensure => directory,
-   owner => 'root',
-   group => 'root',
-   mode => '0775',
-}
-file { '/etc/nginx/conf.d/default.conf':
+file { "${confdir}/conf.d/default.conf":
    ensure => file,
-   source => 'puppet:///modules/nginx/default.conf',
-   require => Package['nginx'],
+   content => template('nginx/default.conf.erb'),
    notify => Service['nginx'],
 }
 service { 'nginx':
    ensure => running,
    enable => true,
+ }
 }
-}
-#Homework 11.2: Proposed Solution
+#Homework 15.2: Proposed Solution
 #VirtualFundamentals
